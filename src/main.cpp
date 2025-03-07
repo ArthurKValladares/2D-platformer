047793501c7f8b62f5239f770314bee47524c54d
ArthurKValladares <arthurkaukal@gmail.com>
2025-03-06 22:53:09 -0500

    init all basic non-pipeline structures

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,38 @@
 
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
+#include <SDL3/SDL_vulkan.h>
+
+#include <vector>
+#include <iostream>
+
+namespace {
+    static inline void chk(VkResult result) {
+        if (result != VK_SUCCESS) {
+            std::cerr << "Vulkan call returned an error\n";
+            exit(result);
+        }
+    }
+    static inline void chk(bool result) {
+        if (!result) {
+            std::cerr << "Call returned an error\n";
+            exit(result);
+        }
+    }
+    static inline void chk(HRESULT result) {
+        if (FAILED(result)) {
+            std::cerr << "Call returned an error\n";
+            exit(result);
+        }
+    }
+
+    static inline void chk_sdl(bool result) {
+        if (!result) {
+            std::cerr << SDL_GetError() << std::endl;
+            exit(result);
+        }
+    }
+};
 
 int main(int argc, char *argv[]) {
     //
@@ -17,17 +49,221 @@ int main(int argc, char *argv[]) {
     SDL_Window* window = nullptr;
     SDL_Surface* screen_surface = nullptr;
 
-    if(SDL_Init(SDL_INIT_VIDEO) < 0) {
-        SDL_Log("SDL could not initialize! SDL error: %s\n", SDL_GetError());
-        return -1;
-    }
+    chk_sdl(SDL_Init(SDL_INIT_VIDEO));
 
-    window = SDL_CreateWindow("2D-Platformer", SCREEN_WIDTH, SCREEN_HEIGHT, 0);
+    const int window_flags = SDL_WINDOW_VULKAN;
+    window = SDL_CreateWindow("2D-Platformer", SCREEN_WIDTH, SCREEN_HEIGHT, window_flags);
     if(window == NULL) {
         SDL_Log("Window could not be created! SDL error: %s\n", SDL_GetError());
         return -1;
     }
     screen_surface = SDL_GetWindowSurface(window);
+
+    //
+    // Init Vulkan
+    //
+    constexpr uint32_t max_frames_in_flight = 2;
+    const VkSampleCountFlagBits sample_count = VK_SAMPLE_COUNT_4_BIT;
+    uint32_t image_index = 0;
+    uint32_t frame_index = 0;
+
+    VkInstance instance = VK_NULL_HANDLE;
+    VkDevice device = VK_NULL_HANDLE;
+    VkQueue queue = VK_NULL_HANDLE;
+
+    VmaAllocator allocator = VK_NULL_HANDLE;
+
+    VkSurfaceKHR surface = VK_NULL_HANDLE;
+
+    VkSwapchainKHR swapchain = VK_NULL_HANDLE;
+    std::vector<VkImage> swapchain_images;
+    std::vector<VkImageView> swapchain_image_views;
+
+    VkImage render_image;
+    VmaAllocation render_image_allocation;
+    VkImageView render_image_view;
+
+    VmaAllocation v_buffer_allocation = VK_NULL_HANDLE;
+    VkBuffer v_buffer = VK_NULL_HANDLE;
+
+    VkCommandPool command_pool = VK_NULL_HANDLE;
+
+    std::vector<VkCommandBuffer> command_buffers(max_frames_in_flight);
+    std::vector<VkFence> fences(max_frames_in_flight);
+    std::vector<VkSemaphore> present_semaphores(max_frames_in_flight);
+    std::vector<VkSemaphore> render_semaphores(max_frames_in_flight);
+
+    VkPipeline pipeline = VK_NULL_HANDLE;
+    VkPipelineLayout pipeline_layout = VK_NULL_HANDLE;
+
+    // Instance
+    volkInitialize();
+	VkApplicationInfo app_info = {
+        .sType = VK_STRUCTURE_TYPE_APPLICATION_INFO,
+        .pApplicationName = "2D-Platformer",
+        .apiVersion = VK_API_VERSION_1_3
+    };
+	const std::vector<const char*> instance_extensions = {
+        VK_KHR_SURFACE_EXTENSION_NAME,
+        VK_KHR_WIN32_SURFACE_EXTENSION_NAME
+    };
+	VkInstanceCreateInfo instance_ci = {
+		.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO,
+		.pApplicationInfo = &app_info,
+		.enabledExtensionCount = static_cast<uint32_t>(instance_extensions.size()),
+		.ppEnabledExtensionNames = instance_extensions.data(),
+	};
+	chk(vkCreateInstance(&instance_ci, nullptr, &instance));
+	volkLoadInstance(instance);
+
+    // Device
+    uint32_t device_count = 0;
+    vkEnumeratePhysicalDevices(instance, &device_count, nullptr);
+    std::vector<VkPhysicalDevice> devices(device_count);
+    vkEnumeratePhysicalDevices(instance, &device_count, devices.data());
+    const uint32_t q_family_idx = 0;
+    const float q_family_prios = 1.0f;
+    const uint32_t device_index = 0;
+    VkDeviceQueueCreateInfo queue_ci = {
+        .sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO,
+        .queueFamilyIndex = q_family_idx,
+        .queueCount = 1,
+        .pQueuePriorities = &q_family_prios
+    };
+    VkPhysicalDeviceVulkan13Features features = {
+        .sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_VULKAN_1_3_FEATURES,
+        .dynamicRendering = true
+    };
+    const std::vector<const char*> device_extensions = {
+        VK_KHR_SWAPCHAIN_EXTENSION_NAME
+    };
+    VkDeviceCreateInfo device_ci = {
+        .sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO,
+        .pNext = &features,
+        .queueCreateInfoCount = 1,
+        .pQueueCreateInfos = &queue_ci,
+        .enabledExtensionCount = static_cast<uint32_t>(device_extensions.size()),
+        .ppEnabledExtensionNames = device_extensions.data(),
+    };
+    chk(vkCreateDevice(devices[device_index], &device_ci, nullptr, &device));
+    vkGetDeviceQueue(device, q_family_idx, 0, &queue);
+
+    // VMA
+	VmaVulkanFunctions vk_functions = {
+        .vkGetInstanceProcAddr = vkGetInstanceProcAddr,
+        .vkGetDeviceProcAddr = vkGetDeviceProcAddr,
+        .vkCreateImage = vkCreateImage
+    };
+	VmaAllocatorCreateInfo allocator_ci = {
+        .physicalDevice = devices[device_index],
+        .device = device,
+        .pVulkanFunctions = &vk_functions,
+        .instance = instance
+    };
+	chk(vmaCreateAllocator(&allocator_ci, &allocator));
+
+    // Presentation
+    chk_sdl(SDL_Vulkan_CreateSurface(window, instance, nullptr, &surface));
+    int window_width;
+    int window_height;
+    SDL_GetWindowSize(window, &window_width, &window_height);
+    const VkFormat image_format = VK_FORMAT_B8G8R8A8_SRGB;
+    const VkColorSpaceKHR color_space = VK_COLORSPACE_SRGB_NONLINEAR_KHR;
+    VkSwapchainCreateInfoKHR swapchainCI = {
+        .sType = VK_STRUCTURE_TYPE_SWAPCHAIN_CREATE_INFO_KHR,
+        .surface = surface,
+        .minImageCount = 2,
+        .imageFormat = image_format,
+        .imageColorSpace = color_space,
+        .imageExtent = {.width = (uint32_t) window_width, .height = (uint32_t) window_height},
+        .imageArrayLayers = 1,
+        .imageUsage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT | VK_IMAGE_USAGE_TRANSFER_DST_BIT,
+        .queueFamilyIndexCount = q_family_idx,
+        .preTransform = VK_SURFACE_TRANSFORM_IDENTITY_BIT_KHR,
+        .compositeAlpha = VK_COMPOSITE_ALPHA_OPAQUE_BIT_KHR,
+        .presentMode = VK_PRESENT_MODE_FIFO_KHR
+
+    };
+    chk(vkCreateSwapchainKHR(device, &swapchainCI, nullptr, &swapchain));
+    uint32_t image_count{ 0 };
+    vkGetSwapchainImagesKHR(device, swapchain, &image_count, nullptr);
+    swapchain_images.resize(image_count);
+    vkGetSwapchainImagesKHR(device, swapchain, &image_count, swapchain_images.data());
+    swapchain_image_views.resize(image_count);
+    VkImageCreateInfo renderImageCI = {
+        .sType = VK_STRUCTURE_TYPE_IMAGE_CREATE_INFO,
+        .imageType = VK_IMAGE_TYPE_2D,
+        .format = image_format,
+        .extent = {.width = (uint32_t) window_width, .height = (uint32_t) window_height, .depth = 1 },
+        .mipLevels = 1,
+        .arrayLayers = 1,
+        .samples = sample_count,
+        .tiling = VK_IMAGE_TILING_OPTIMAL,
+        .usage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT | VK_IMAGE_USAGE_TRANSFER_SRC_BIT,
+        .initialLayout = VK_IMAGE_LAYOUT_UNDEFINED,
+    };
+    VmaAllocationCreateInfo allocCI = {
+        .flags = VMA_ALLOCATION_CREATE_DEDICATED_MEMORY_BIT,
+        .usage = VMA_MEMORY_USAGE_AUTO,
+        .priority = 1.0f
+    };
+    vmaCreateImage(allocator, &renderImageCI, &allocCI, &render_image, &render_image_allocation, nullptr);
+    VkImageViewCreateInfo view_ci = {
+        .sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO,
+        .image = render_image,
+        .viewType = VK_IMAGE_VIEW_TYPE_2D,
+        .format = image_format,
+        .subresourceRange = {
+            .aspectMask = VK_IMAGE_ASPECT_COLOR_BIT,
+            .levelCount = 1,
+            .layerCount = 1
+        }
+    };
+    chk(vkCreateImageView(device, &view_ci, nullptr, &render_image_view));
+    for (auto i = 0; i < image_count; i++) {
+        view_ci.image = swapchain_images[i];
+        chk(vkCreateImageView(device, &view_ci, nullptr, &swapchain_image_views[i]));
+    }
+
+    // Vertexbuffer (Pos 3f, Col 3f)
+    const std::vector<float> vertices = {
+        0.0f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
+        0.5f, 0.5f, 0.0f, 0.0f, 1.0f, 0.0f,
+        -0.5f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f
+    };
+    VkBufferCreateInfo buffer_ci = {
+        .sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO,
+        .size = sizeof(float) * vertices.size(),
+        .usage = VK_BUFFER_USAGE_VERTEX_BUFFER_BIT
+    };
+    VmaAllocationCreateInfo buffer_alloc_ci = {
+        .flags = VMA_ALLOCATION_CREATE_HOST_ACCESS_SEQUENTIAL_WRITE_BIT | VMA_ALLOCATION_CREATE_HOST_ACCESS_ALLOW_TRANSFER_INSTEAD_BIT | VMA_ALLOCATION_CREATE_MAPPED_BIT,
+        .usage = VMA_MEMORY_USAGE_AUTO
+    };
+    chk(vmaCreateBuffer(allocator, &buffer_ci, &buffer_alloc_ci, &v_buffer, &v_buffer_allocation, nullptr));
+    void* buffer_ptr = nullptr;
+    vmaMapMemory(allocator, v_buffer_allocation, &buffer_ptr);
+    memcpy(buffer_ptr, vertices.data(), sizeof(float)* vertices.size());
+    vmaUnmapMemory(allocator, v_buffer_allocation);
+
+    // Command Pool
+    VkCommandPoolCreateInfo command_pool_ci = {
+        .sType = VK_STRUCTURE_TYPE_COMMAND_POOL_CREATE_INFO,
+        .flags = VK_COMMAND_POOL_CREATE_RESET_COMMAND_BUFFER_BIT,
+        .queueFamilyIndex = q_family_idx
+};
+    chk(vkCreateCommandPool(device, &command_pool_ci, nullptr, &command_pool));
+
+    // Sync objects
+    for (auto i = 0; i < max_frames_in_flight; i++) {
+        VkCommandBufferAllocateInfo command_buffer_ai = { .sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO, .commandPool = command_pool, .commandBufferCount = 1 };
+        chk(vkAllocateCommandBuffers(device, &command_buffer_ai, &command_buffers[i]));
+        VkFenceCreateInfo fence_ci = { .sType = VK_STRUCTURE_TYPE_FENCE_CREATE_INFO, .flags = VK_FENCE_CREATE_SIGNALED_BIT };
+        vkCreateFence(device, &fence_ci, nullptr, &fences[i]);
+        VkSemaphoreCreateInfo semaphore_ci = { .sType = VK_STRUCTURE_TYPE_SEMAPHORE_CREATE_INFO };
+        chk(vkCreateSemaphore(device, &semaphore_ci, nullptr, &present_semaphores[i]));
+        chk(vkCreateSemaphore(device, &semaphore_ci, nullptr, &render_semaphores[i]));
+    }
 
     //
     // Main loop
@@ -47,6 +283,28 @@ int main(int argc, char *argv[]) {
             }
         }
     }
+
+    // Cleanup Vulkan
+    vkDeviceWaitIdle(device);
+    for (auto i = 0; i < max_frames_in_flight; i++) {
+        vkDestroyFence(device, fences[i], nullptr);
+        vkDestroySemaphore(device, present_semaphores[i], nullptr);
+        vkDestroySemaphore(device, render_semaphores[i], nullptr);
+    }
+    vmaDestroyImage(allocator, render_image, render_image_allocation);
+    vkDestroyImageView(device, render_image_view, nullptr);
+    for (auto i = 0; i < swapchain_image_views.size(); i++) {
+        vkDestroyImageView(device, swapchain_image_views[i], nullptr);
+    }
+    vmaDestroyBuffer(allocator, v_buffer, v_buffer_allocation);
+    vkDestroyCommandPool(device, command_pool, nullptr);
+    //vkDestroyPipelineLayout(device, pipeline_layout, nullptr);
+    //vkDestroyPipeline(device, pipeline, nullptr);
+    vkDestroySwapchainKHR(device, swapchain, nullptr);
+    vkDestroySurfaceKHR(instance, surface, nullptr);
+    vmaDestroyAllocator(allocator);
+    vkDestroyDevice(device, nullptr);
+    vkDestroyInstance(instance, nullptr);
 
     //
     // Cleanup SDL
