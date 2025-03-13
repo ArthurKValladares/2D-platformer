421b544830c73a74f0b84b5d2c17e453e565cb08
ArthurKValladares <arthurkaukal@gmail.com>
2025-03-12 22:28:43 -0400

    get more data from shader

--- src/renderer/shader.cpp
+++ src/renderer/shader.cpp
@@ -11,9 +11,41 @@ ShaderData::ShaderData(size_t size, const void* p_code) {
     uint32_t var_count = 0;
     result = spvReflectEnumerateInputVariables(&module, &var_count, NULL);
     assert(result == SPV_REFLECT_RESULT_SUCCESS);
-    interface_variables.resize(var_count);
-    result = spvReflectEnumerateInputVariables(&module, &var_count, &interface_variables[0]);
+    if (var_count != 0) {
+        input_vars.resize(var_count);
+        result = spvReflectEnumerateInputVariables(&module, &var_count, &input_vars[0]);
+        assert(result == SPV_REFLECT_RESULT_SUCCESS);
+    }
+
+    // Enumerate and extract shader's output variables
+    var_count = 0;
+    result = spvReflectEnumerateOutputVariables(&module, &var_count, NULL);
     assert(result == SPV_REFLECT_RESULT_SUCCESS);
+    if (var_count != 0) {
+        output_vars.resize(var_count);
+        result = spvReflectEnumerateOutputVariables(&module, &var_count, &output_vars[0]);
+        assert(result == SPV_REFLECT_RESULT_SUCCESS);
+    }
+
+    // Enumerate and extract shader's Descriptor bindings
+    var_count = 0;
+    result = spvReflectEnumerateDescriptorBindings(&module, &var_count, NULL);
+    assert(result == SPV_REFLECT_RESULT_SUCCESS);
+    if (var_count != 0) {
+        descriptor_bindings.resize(var_count);
+        result = spvReflectEnumerateDescriptorBindings(&module, &var_count, &descriptor_bindings[0]);
+        assert(result == SPV_REFLECT_RESULT_SUCCESS);
+    }
+    
+    // Enumerate and extract shader's Descriptor sets
+    var_count = 0;
+    result = spvReflectEnumerateDescriptorSets(&module, &var_count, NULL);
+    assert(result == SPV_REFLECT_RESULT_SUCCESS);
+    if (var_count != 0) {
+        descriptor_sets.resize(var_count);
+        result = spvReflectEnumerateDescriptorSets(&module, &var_count, &descriptor_sets[0]);
+        assert(result == SPV_REFLECT_RESULT_SUCCESS);
+    }
 }
 
 ShaderData::~ShaderData() {
--- src/renderer/shader.h
+++ src/renderer/shader.h
@@ -7,7 +7,10 @@
 struct ShaderData {
     ShaderData(size_t size, const void* p_code);
     ~ShaderData();
-    
+
     SpvReflectShaderModule module;
-    std::vector<SpvReflectInterfaceVariable*> interface_variables;
+    std::vector<SpvReflectInterfaceVariable*> input_vars;
+    std::vector<SpvReflectInterfaceVariable*> output_vars;
+    std::vector<SpvReflectDescriptorBinding*> descriptor_bindings;
+    std::vector<SpvReflectDescriptorSet*> descriptor_sets;
 };
